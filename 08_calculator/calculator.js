bfc991dde06f765c7ffb2afa36ad9fd546cee0af
jumpyguest <roland.almendras.salazar@gmail.com>
2025-01-15 11:05:10 +0800

    Complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return +a + +b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, current) =>  total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product, item) => product * item, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let product = a;
+	for (let x = 1; x < b; x++) {
+    product *= a;
+  }
+  return product;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a === 0) {
+    return 1;
+  }
+  let product = 1;
+	for (let x = 1; x <= a; x++) {
+    product *= x;
+  }
+  return product;
 };
 
 // Do not edit below this line
