d9870f28ea449b2743bfc753e48876bd3c73147e
jumpyguest <roland.almendras.salazar@gmail.com>
2025-01-15 13:01:17 +0800

    Solve exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let array = string.trim().toUpperCase().split('').filter(isLetterNum);
 
+    function isLetterNum(char) {
+        return ((char >= 'A' && char <= 'Z') || 
+                (char >= '0' && char <= '9'));
+    }
+
+    let reversed = array.slice().reverse();
+    return array.every((value, index) => value === reversed[index]);
 };
-
 // Do not edit below this line
 module.exports = palindromes;
