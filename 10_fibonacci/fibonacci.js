506b3b98988906771f22e695924a225ebbb6501e
jumpyguest <roland.almendras.salazar@gmail.com>
2025-01-16 00:02:26 +0800

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let intNum = parseInt(num);
+    let sequence = [1];
+    let sum = 0;
+    if (intNum === 0) {
+        return 0;
+    } else if (intNum < 0) {
+        return "OOPS";
+    }
+    for(let x = 0; x < intNum-1; x++) {
+        //let last = sequence.reduce((sum, item) => sum + item, 0);
+        sequence[x+1] = sum + sequence[x];
+        sum = sequence[x];
+    }   
+    return sequence[intNum-1];
 };
 
 // Do not edit below this line
